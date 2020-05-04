afef3ef9226d1b2492884cfd08b623a36eacb7a5
EsTOEban <kevin.haynes.dev@gmail.com>
2020-05-04 06:06:16 +0000

    Updates

---
+++ precache-manifest.1d4c9b250ab6b78df5bc36c93644707b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5561c5968bd06cd94f5b5b1eb1090b94",
+    "url": "/personal-react-site/index.html"
+  },
+  {
+    "revision": "a419b94063ebff34ce7a",
+    "url": "/personal-react-site/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "095fe12c3b3b1a96af56",
+    "url": "/personal-react-site/static/js/2.6801dbf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-react-site/static/js/2.6801dbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a419b94063ebff34ce7a",
+    "url": "/personal-react-site/static/js/main.48ecbdaf.chunk.js"
+  },
+  {
+    "revision": "ae5c2c4097d724259fb5",
+    "url": "/personal-react-site/static/js/runtime-main.e1d9c5c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82fb06433c9a3777463677800d3f9bd0.js"
+  "/personal-react-site/precache-manifest.1d4c9b250ab6b78df5bc36c93644707b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-react-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
